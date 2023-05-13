61dd67a35bd28b65a2333594a45569bd3b5a1810
koi <yennhi135135@example.com>
2023-05-13 16:19:37 +0700

    table

--- admin/inventory/index.php
+++ admin/inventory/index.php
@@ -79,6 +79,7 @@
                     lengthMenu: "Hiển thị _MENU_ dòng mỗi trang",
                     search: "Tìm kiếm:",
                     info: "Hiển thị _START_ đến _END_ của _TOTAL_ dòng",
+					
                 }
             });
 	})
--- my_account.php
+++ my_account.php
@@ -96,6 +96,10 @@
                     lengthMenu: "Hiển thị _MENU_ dòng mỗi trang",
                     search: "Tìm kiếm:",
                     info: "Hiển thị _START_ đến _END_ của _TOTAL_ dòng",
+                    paginate: {
+                        "previous": "Trước",
+                        "next": "Sau"}
+                   
                 }
             });
 
